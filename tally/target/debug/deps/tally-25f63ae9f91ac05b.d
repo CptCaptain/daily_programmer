e0eb2e2525fba312f2c48b77e17c7457f8f0eb79
Cpt. Captain <nilskoch94@gmail.com>
2018-05-25 18:11:54 +0200

    removed unnecessary bloating files

--- tally/target/debug/deps/tally-25f63ae9f91ac05b.d
+++
@@ -1,5 +0,0 @@
-/home/hans/Code/rust/daily/tally/target/debug/deps/tally-25f63ae9f91ac05b: src/main.rs
-
-/home/hans/Code/rust/daily/tally/target/debug/deps/tally-25f63ae9f91ac05b.d: src/main.rs
-
-src/main.rs:
--- tally/target/debug/tally.d
+++
@@ -1,1 +0,0 @@
-/home/hans/Code/rust/daily/tally/target/debug/tally: /home/hans/Code/rust/daily/tally/src/main.rs
